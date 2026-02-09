eaa10613cd04873e837e3213d07fc542b7e56b46
Pavan Shinde <pavann97@gmail.com>
2026-02-09 11:08:07 +0000

    test(table-core): make perf regression test opt-in

--- packages/table-core/tests/getGroupedRowModel.test.ts
+++ packages/table-core/tests/getGroupedRowModel.test.ts
@@ -18,35 +18,40 @@ function generateColumns(people: Person[]): PersonColumn[] {
 }
 
 describe('#getGroupedRowModel', () => {
-  it('groups 50k rows and 3 grouped columns with clustered data in less than 5 seconds', () => {
-    const data = makeData(50000)
-    const columns = generateColumns(data)
-    const grouping = ['firstName', 'lastName', 'age']
-    const start = new Date()
+  // This is a performance regression test that can be noisy on shared CI runners.
+  // Run locally/explicitly with: TANSTACK_PERF_TESTS=1 pnpm test
+  it.runIf(Boolean(process.env.TANSTACK_PERF_TESTS))(
+    'groups 50k rows and 3 grouped columns with clustered data in less than 5 seconds',
+    () => {
+      const data = makeData(50000)
+      const columns = generateColumns(data)
+      const grouping = ['firstName', 'lastName', 'age']
+      const start = new Date()
 
-    data.forEach((p) => (p.firstName = 'Fixed'))
-    data.forEach((p) => (p.lastName = 'Name'))
-    data.forEach((p) => (p.age = 123))
+      data.forEach((p) => (p.firstName = 'Fixed'))
+      data.forEach((p) => (p.lastName = 'Name'))
+      data.forEach((p) => (p.age = 123))
 
-    const table = createTable<Person>({
-      onStateChange() {},
-      renderFallbackValue: '',
-      data,
-      state: { grouping },
-      columns,
-      getCoreRowModel: getCoreRowModel(),
-      getGroupedRowModel: getGroupedRowModel(),
-    })
-    const groupedById = table.getGroupedRowModel().rowsById
-    const end = new Date()
+      const table = createTable<Person>({
+        onStateChange() {},
+        renderFallbackValue: '',
+        data,
+        state: { grouping },
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        getGroupedRowModel: getGroupedRowModel(),
+      })
+      const groupedById = table.getGroupedRowModel().rowsById
+      const end = new Date()
 
-    expect(groupedById['firstName:Fixed'].leafRows.length).toEqual(50000)
-    expect(
-      groupedById['firstName:Fixed>lastName:Name'].leafRows.length,
-    ).toEqual(50000)
-    expect(
-      groupedById['firstName:Fixed>lastName:Name>age:123'].leafRows.length,
-    ).toEqual(50000)
-    expect(end.valueOf() - start.valueOf()).toBeLessThan(5000)
-  })
+      expect(groupedById['firstName:Fixed'].leafRows.length).toEqual(50000)
+      expect(
+        groupedById['firstName:Fixed>lastName:Name'].leafRows.length,
+      ).toEqual(50000)
+      expect(
+        groupedById['firstName:Fixed>lastName:Name>age:123'].leafRows.length,
+      ).toEqual(50000)
+      expect(end.valueOf() - start.valueOf()).toBeLessThan(5000)
+    },
+  )
 })
